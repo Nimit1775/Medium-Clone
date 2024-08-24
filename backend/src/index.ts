2a732b7f03be773c0a45b1a20bcc4d103ef0e4fd
nimit sodhani <nimitsodhani1@gmail.com>
2024-08-24 19:15:41 +0530

    prisma migrations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
 import { Hono } from 'hono'
 
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
 const app = new Hono()
 
 app.post(' /api/v1/user/signup', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
+
   return c.text('signup route')
 })
 
