9f886834aca948726d726cc3cf5c4792ccef4c5e
nimit sodhani <nimitsodhani1@gmail.com>
2024-08-26 01:04:56 +0530

    hono env error fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,15 @@ import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings : {
+    DATABASE_URL: string
+  }
+}>() ; 
 
 app.post(' /api/v1/user/signup', (c) => {
   const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
+    datasourceUrl: c.env.DATABASE_URL ,
 }).$extends(withAccelerate())
 
   return c.text('signup route')
