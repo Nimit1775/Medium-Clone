5f7a35862ac47b2434c8a01665ae66a5288b27f9
nimit sodhani <nimitsodhani1@gmail.com>
2024-08-28 18:28:13 +0530

    starting blog routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,4 @@
 import { Hono } from 'hono'
-import {sign , verify} from 'hono/jwt' 
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
 
@@ -15,21 +12,6 @@ const app = new Hono<{
 app.route("/api/v1/user" , userRouter) ; 
 app.route("api/v1/blog" , blogRouter) ;
 
-
-
-
-app.post('/api/v1/blog', (c) => {
-  return c.text(' post blog route')
-})
-app.put('/api/v1/blog', (c) => {
-  return c.text(' put blog route')
-})
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text(' get blog route')
-})
-app.get('/' , (c)=> {
-  return c.text('Welcome to Hono')
-})
 
 
 export default app
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,3 +1,16 @@
 import { Hono } from "hono";
 
 export const blogRouter = new Hono()  ; 
+
+blogRouter.post('/', (c) => {
+    return c.text(' post blog route')
+  })
+  blogRouter.put('/', (c) => {
+    return c.text(' put blog route')
+  })
+  blogRouter.get('/', (c) => {
+    return c.text(' get blog route')
+  })
+  blogRouter.get('/bulk', (c) => {
+    return c.text(' get blog route')
+  })
