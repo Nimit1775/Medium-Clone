97e115879584a57d04e50cfc11a4f32a835d1a71
nimit sodhani <nimitsodhani1@gmail.com>
2024-09-04 18:23:54 +0530

    working on blogs components

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,6 +1,14 @@
+import { BlogCard } from "../components/BlogCard"
 
 export const Blogs = () => {
   return (
-    <div>Blogs</div>
+    <div>
+      <BlogCard
+      authorName = {"nimit"}
+      title= {"My first blog"}
+      content = {"This is my first blog. I am writing this blog to test the blog card component. I hope it works fine."}
+      publishedDate ={ "2021-09-01" }
+      /> 
+    </div>
   )
 }
