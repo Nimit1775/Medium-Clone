219f1ebed61bfece347dfaf6b85a4c38320b46ea
nimit sodhani <nimitsodhani1@gmail.com>
2024-09-05 22:46:57 +0530

    some fixes

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,13 +1,14 @@
-import { Avatar } from "./BlogCard"
+import { Avatar } from "./BlogCard";
+
 
 export const Appbar = () => {
-  return <div className="border-b flex justify-between px-10 py-4 ">
-        <div className="font-bold text-lg flex flex-col justify-center">
-                Medium 
-        </div>
-        <div>
-            <Avatar size={5} name="Nimit"/>
-        </div>
-  </div>
+  return <div className="border-b flex justify-between px-10 py-4">
+      <div className="font-bold text-lg flex flex-col justify-center">
+        Medium
+      </div>
+      <div>
+        <Avatar size={2} name="Nimit" />
+      </div>
+    </div>
 
-}
+};
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -48,13 +48,15 @@ function Dot ()
   </div>
 }
 
- export function Avatar ({ name , size=5 }:{ name: string  , size ?: number}) {
-  return <div className={`relative inline-flex items-center 
-    justify-center w-${size} h-${size}  overflow-hidden
-   bg-gray-100 rounded-full dark:bg-gray-600`}>
-    <span className="font-xs font-thin text-gray-600 dark:text-gray-300">
-      {name[0]}
-    </span>
-</div>
-
+export function Avatar({ name, size = 1 }: { name: string; size?: number }) {
+  return (
+    <div
+      className={`relative inline-flex items-center justify-center overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}
+      style={{ width: `${size}rem`, height: `${size}rem` }} 
+    >
+      <span className="font-xs font-thin text-gray-600 dark:text-gray-300">
+        {name[0]}
+      </span>
+    </div>
+  );
 }
