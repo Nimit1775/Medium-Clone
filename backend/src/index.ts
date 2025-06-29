6b285743f5cc65a5ec7538585f5c267bfdafad51
nimit sodhani <nimitsodhani1@gmail.com>
2025-06-29 15:36:24 +0530

    refractored code for hono + cloudfare new updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,19 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
-import { cors } from 'hono/cors'
+import { cors } from 'hono/cors';
+
 const app = new Hono<{
-  Bindings : {
-    DATABASE_URL: string , 
-    JWT_SECRET : string ,  
-  }
-}>() ; 
-app.use('/*' , cors()) ;
-app.route("/api/v1/user" , userRouter) ; 
-app.route("api/v1/blog" , blogRouter) ;
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
+app.use('/*', cors());
+app.route('/api/v1/user', userRouter);
+app.route('/api/v1/blog', blogRouter);
 
-
-export default app
+export default {
+  fetch: app.fetch,
+};
